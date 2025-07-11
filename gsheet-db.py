4de61676f9a02e21712faa907759a179e17ee1cc
josephbonyemmanuel <64676279+josephbonyemmanuel@users.noreply.github.com>
2025-07-11 14:19:10 +0530

    Update gsheet-db.py

--- gsheet-db.py
+++ gsheet-db.py
@@ -1,23 +1,5 @@
 import streamlit as st
 
-# 🔒 Login gate
-def check_password():
-    def password_entered():
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["authenticated"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["authenticated"] = False
-
-    if "authenticated" not in st.session_state:
-        st.text_input("🔒 Enter Password", type="password", on_change=password_entered, key="password")
-        st.stop()
-    elif not st.session_state["authenticated"]:
-        st.error("❌ Incorrect password")
-        st.stop()
-
-check_password()
-
 import pandas as pd
 import datetime
 
